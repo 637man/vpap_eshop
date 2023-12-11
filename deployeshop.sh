8e73e99305570670b37e8d34122268dec6c5df59
Johannes Getmann <jan.getmancuk@gmail.com>
2023-12-11 11:32:09 +0100

    přesunuty a očíslovány SQL soubory

--- deployeshop.sh
+++ deployeshop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Zkopiruje lokalni kopii repozare na produkci, doda kredencialy DB
+# a nastavi opravneni, aby PHP demon mohl zapisovat.
 cd ~
 cp -r vpap_eshop public_html
 cp eshopconfiglocal.neon public_html/vpap_eshop/config/local.neon
